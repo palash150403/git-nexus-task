cbfa1572daea9c8de3ac6b0e10f3f0e3d9327214
Palash Gajjar <narendramodihe@gmail.com>
2024-12-03 16:05:44 +0530

    testing tag on main 1.1

--- obj/Debug/net8.0/helloworld.AssemblyInfo.cs
+++ obj/Debug/net8.0/helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bbf299e59ecb5eab21c9e883384426e7e8e6e1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fe1255f4710e7848c5bded694158d9683272aee")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
