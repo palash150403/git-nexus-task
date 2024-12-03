fe01825c4fa38566db79e07880623e65888a722c
Palash Gajjar <narendramodihe@gmail.com>
2024-12-03 19:08:26 +0530

    stage

--- obj/Debug/net8.0/helloworld.AssemblyInfo.cs
+++ obj/Debug/net8.0/helloworld.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+956f58724f6a65d04002a5f599727f2130fd236e")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1047e10fbe7a6e9eedb0b8b564892fe4719465ef")]
+>>>>>>> 05b1c0c (stage release)
 [assembly: System.Reflection.AssemblyProductAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
