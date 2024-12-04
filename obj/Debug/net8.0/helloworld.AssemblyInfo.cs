1f84e332c043ec7c6607b46b4e5501cfb26ba218
Palash Gajjar <narendramodihe@gmail.com>
2024-12-04 14:24:16 +0530

    release test

--- obj/Debug/net8.0/helloworld.AssemblyInfo.cs
+++ obj/Debug/net8.0/helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87f50c89848ea2ab298ec0e2064b7eb9ed5251e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d29b2101fe6833ab851d58fc47d63457cd9eb3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
